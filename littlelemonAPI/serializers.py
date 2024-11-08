01bc998a8bac3ee61eadc3e46a0c2720d4ccbd9a
engenovic <engenovic@gmail.com>
2024-11-08 10:35:59 +0300

    Added serializers

--- littlelemonAPI/serializers.py
+++ littlelemonAPI/serializers.py
@@ -1,7 +1,32 @@
 from rest_framework import serializers
-from .models import MenuItem
+from .models import MenuItem, Category
+from decimal import Decimal
 
+#Starter Serializer
+# class MenuItemSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = MenuItem
+#         fields = ['id','title','price','inventory']
+
+# #Normal Serializer
+# class MenuItemSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=255)
+#     price = serializers.DecimalField(max_digits=6,decimal_places=2)
+#     inventory = serializers.IntegerField()
+
+#Model Serializer
+class CategorySerilizer (serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id','slug','title']
 class MenuItemSerializer(serializers.ModelSerializer):
+    stock = serializers.IntegerField(source='inventory') # Change inventory to stock field
+    price_after_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+   # category = serializers.StringRelatedField() # Relationship Serializer
+    category = CategorySerilizer()
     class Meta:
         model = MenuItem
-        fields = ['id','title','price','inventory']
+        fields = ['id','title','category','price','stock','price_after_tax']
+    def calculate_tax(self,product:MenuItem): # Add calculated fields to the serializer
+        return product.price * Decimal(1.1)
--- littlelemonAPI/views.py
+++ littlelemonAPI/views.py
@@ -1,11 +1,28 @@
-from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from .models import MenuItem
 from .serializers import MenuItemSerializer
+from  django.shortcuts import get_object_or_404
 
-class MenuItemsView(generics.ListCreateAPIView):
-    queryset = MenuItem.objects.all()
-    serializer_class = MenuItemSerializer
+# Quick Starter for DRF - All Items
+# class MenuItemsView(generics.ListCreateAPIView):
+#     queryset = MenuItem.objects.all()
+#     serializer_class = MenuItemSerializer
 
-class SingleMenuItemView(generics.RetrieveUpdateAPIView,generics.DestroyAPIView):
-    queryset = MenuItem.objects.all()
-    serializer_class = MenuItemSerializer
+# Quick Starter for DRF - Single Item
+# class SingleMenuItemView(generics.RetrieveUpdateAPIView,generics.DestroyAPIView):
+#     queryset = MenuItem.objects.all()
+#     serializer_class = MenuItemSerializer
+
+
+@api_view()
+def menu_items(request):
+    items = MenuItem.objects.select_related('category').all() # Select related data in single SQL Call
+    serialized_item = MenuItemSerializer(items,many=True)
+    return Response(serialized_item.data)
+
+@api_view()
+def single_item(request,id):
+    item = get_object_or_404(MenuItem,pk=id)
+    serialized_item = MenuItemSerializer(item)
+    return Response(serialized_item.data)
