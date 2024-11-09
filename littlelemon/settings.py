30f37d704d4af5440d4bec2ff9e02aacbdf05ee7
engenovic <engenovic@gmail.com>
2024-11-09 12:20:57 +0300

    AUth Token implementation

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'littlelemonAPI',
 ]
 
@@ -117,5 +118,8 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication', 
+    ],
 }
--- littlelemonAPI/views.py
+++ littlelemonAPI/views.py
@@ -4,6 +4,11 @@ from .models import MenuItem
 from .serializers import MenuItemSerializer
 from rest_framework import status
 from  django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator, EmptyPage
+
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
+
 
 # Quick Starter for DRF - All Items
 # class MenuItemsView(generics.ListCreateAPIView):
@@ -20,7 +25,27 @@ from  django.shortcuts import get_object_or_404
 def menu_items(request):
     if request.method=='GET':
         items = MenuItem.objects.select_related('category').all() # Select related data in single SQL Call
-        serialized_item = MenuItemSerializer(items,many=True) # Serialize
+        category_name = request.query_params.get('category') #filter param Category
+        to_price = request.query_params.get('to_price') #filter param Price
+        search = request.query_params.get('search') #search param item title
+        order = request.query_params.get('order') #order param item price
+        perpage = request.query_params.get('perpage',default=2) #perpage param items per page
+        page    = request.query_params.get('page',default=1) #page param page number
+        if category_name:
+            items = items.filter(category__slug=category_name)
+        if to_price:
+            items = items.filter(price__lte=to_price)
+        if search:
+            items = items.filter(title__icontains=search)
+        if order:
+         items = items.order_by(order)
+         
+        paginator = Paginator(items,per_page=perpage)
+        try:
+            items = paginator.page(number=page)
+        except EmptyPage:
+            items= []
+        serialized_item = MenuItemSerializer(items,many=True) # Serialize items
         return Response(serialized_item.data)
     if request.method =='POST':
         deserialized_item = MenuItemSerializer(data=request.data) # DeSerialize 
@@ -34,3 +59,8 @@ def single_item(request,id):
     item = get_object_or_404(MenuItem,pk=id)
     serialized_item = MenuItemSerializer(item)
     return Response(serialized_item.data)
+
+@api_view()
+@permission_classes([IsAuthenticated])
+def secret(request):
+    return Response({"message":"some secret message"})
